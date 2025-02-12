3fd2b6abdb325cede0a5eb034a2e35e9b8e57b01
joacoz29 <joakomartinez29@gmail.com>
2025-02-12 17:18:25 -0300

    Subida proyecto

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,8 +3,8 @@ import { getDatabase } from "firebase/database"
 import { getAuth } from "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH,
   databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
